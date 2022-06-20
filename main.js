30990f8bbf2ab474981804d4da2ab55bcca157e3
이샤 <101727512+22IshaBipte@users.noreply.github.com>
2022-06-20 19:46:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var mouse_Event =""
+var last_position_of_X, last_position_of_Y
+
+canvas=document.getElementById("myCanvas")
+ctx=canvas.getContext("2d")
+
+color="black"
+width=1
+
+canvas.addEventListener("mousedown" , myMouseDown)
+function myMouseDown(){
+    mouse_Event="mousedown"
+
+}
+
+canvas.addEventListener("mouseup" , myMouseUp)
+function myMouseUp(){
+    mouse_Event="mouseup"
+}
+
+canvas.addEventListener("mouseleave" , myMouseLeave)
+function myMouseLeave(){
+    mouse_Event="mouseleave"
+}
+
+canvas.addEventListener("mousemove" , myMouseMove)
+function myMouseMove(e){
+    current_position_of_mouseX=e.clientX-canvas.offsetLeft
+    current_position_of_mouseY=e.clientY-canvas.offsetTop
+    if(mouse_Event=="mousedown"){
+        ctx.beginPath()
+        ctx.strokeStyle=color
+        ctx.lineWidth=width
+        ctx.moveTo(last_position_of_X,last_position_of_Y)
+        ctx.lineTo(current_position_of_mouseX,current_position_of_mouseY)
+        ctx.stroke()
+    }
+
+    last_position_of_X=current_position_of_mouseX
+    last_position_of_Y=current_position_of_mouseY
+}
